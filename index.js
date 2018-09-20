970ef6b754c76d408fc17d02a47b43d0301467f4
Victoria Fluharty <victoria.fluharty@gmail.com>
2018-09-20 00:34:52 +0000

    automatically backed up by learn

--- index.js
+++ index.js
@@ -20,6 +20,6 @@ const driversByRevenue = function (drivers) {
 
 const driversByName = function (drivers) {
   return drivers.slice().sort(function (driverOne, driverTwo) {
-    return driverOne.name - driverTwo.name;
+    return driverOne.name.localeCompare(driverTwo.name);
   });
 }
